25e24c48f94d25380b1aac080b61da3341d84ccb
Akashkr28 <email.akash2010@gmail.com>
2024-07-15 21:44:45 +0530

    selectionJsort

---
+++ selectionSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class selectionSort {
+
+    public static void selectionsort(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            int min_idx = findMinELement(arr, i);
+            if(min_idx != i) {
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+
+public static int findMinElement(int[] arr, int i) {
+    int min_el_idx = i;
+    for(int j = i + 1; j < arr.length; j++){
+        min_el_idx = j;
+    }
+    return min_el_idx;
+}
+
+public static void main(String[] args) {
+    int[] arr = {5,4,3,2,1};
+    selectionsort(arr);
+    for(int i = 0; i < arr.length; i++){
+        System.out.println(arr[i]);
+    }
+}
+}
