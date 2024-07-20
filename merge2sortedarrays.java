7ace2dcce51feed28ba62bc749d9504a16c7cb31
Akashkr28 <email.akash2010@gmail.com>
2024-07-20 21:56:26 +0530

    code updated

--- merge2sortedarrays.java
+++ merge2sortedarrays.java
@@ -3,13 +3,13 @@
 
 public class merge2sortedarrays {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // actual length of the num1 is m+n, which we will use to store the final result
+        // actual length of nums1 is m+n, which we will use to store the final result
 
         int i = 0, j = 0, k = 0;
 
-        int[] result = new int[m+n];
+        int[] result = new int[m + n];
 
-        while( i < m && j < n) { //this loop will ends when either i == m or j == n
+        while ( i < m && j < n) { // this loop ends when either i == m or j == n
             if(nums1[i] <= nums2[j]) {
                 result[k] = nums1[i];
                 k++;
@@ -20,25 +20,29 @@ public class merge2sortedarrays {
                 j++;
             }
         }
+
         // if i == m, nums1 is exhausted
-        //  we have element left in nums2, lets fill that directly
-        while (j < n) {
+        // we have elements left in nums2, lets fill that directly
+        while(j < n) {
             result[k] = nums2[j];
             k++;
             j++;
         }
+
         // if j == n, nums2 is exhausted
         // we have elements left in nums1, lets fill that directly
-        while (i < m) {
+        while(i < m) {
             result[k] = nums1[i];
             k++;
-            j++;
+            i++;
         }
 
         k = 0;
-        while (k < m+n) {
+        while(k < m + n) {
             nums1[k] = result[k];
             k++;
         }
+
+
     }
 }
